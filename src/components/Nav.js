3d89ab1dc0f385e31e70c373e22c266ec4395f64
muthee-p <prumu9@gmail.com>
2023-02-13 08:03:23 +0300

    fourth

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { AiFillGithub } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 const Nav = () =>{
 	return(
 
 		<div className='Nav'>
-			<div>
-				<a href='https://github.com/muthee-p'><AiFillGithub /></a>
+		<div className="nav-container">
+		<div className="logo">
+			<h4><Link to='/'>P</Link></h4>
+		</div>
+		<div className="inner-nav">
+			<div className='nav-cons'>
+				<a href='https://github.com/muthee-p' id="con">
+					<AiFillGithub />
+				</a>
+				<a href="www.linkedin.com/in/muthee-p" id="con">
+					<AiFillLinkedin />
+				</a>
+				<Link to='/about'>About</Link>
+				<Link to='/contact'>Contact</Link>
 			</div>
 			<div className='navs'>
-					<NavLink to='/about'>About</NavLink>
-				
+					{/*<Link to='/about'>About</Link>
+					<Link to='/contact'>Contact</Link>*/}
+			</div>
+			</div>
+			<div className='nav-mail'>
+			<p className='email'><a href="mailto:pruemu9@gmail.com">pruemu9@gmail.com</a></p>
+			</div>
+			
 			</div>
 		</div>
 		);
